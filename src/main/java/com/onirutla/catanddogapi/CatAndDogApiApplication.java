f42b797618211eb61cf180459957412dd75bff38
onirutlA <alturino001@gmail.com>
2021-09-18 23:12:35 +0700

    initial commit

---
+++ src/main/java/com/onirutla/catanddogapi/CatAndDogApiApplication.java
@@ -0,0 +1,13 @@
+package com.onirutla.catanddogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatAndDogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatAndDogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onirutla/catanddogapi/CatAndDogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onirutla.catanddogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatAndDogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
