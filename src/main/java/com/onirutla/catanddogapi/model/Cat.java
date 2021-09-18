88355245633c03510b1a46a2a8b21505e05960ee
onirutlA <alturino001@gmail.com>
2021-09-18 23:42:35 +0700

    add: Cat Model

---
+++ src/main/java/com/onirutla/catanddogapi/model/Cat.java
@@ -0,0 +1,135 @@
+package com.onirutla.catanddogapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CAT")
+public class Cat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String type;
+
+    @Column
+    private String color;
+
+    @Column(name = "owner_id")
+    private String ownerId;
+
+    @Column(name = "owner_name")
+    private String ownerName;
+
+    @Column
+    private String height;
+
+    @Column(name = "created_at")
+    private String createdAt;
+
+    @Column(name = "updated_at")
+    private String updatedAt;
+
+    @Column
+    private Boolean isDeleted;
+
+    public Cat() {
+    }
+
+    public Cat(String id, String name, String type, String color, String ownerId, String ownerName, String height, String createdAt, String updatedAt, Boolean isDeleted) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.color = color;
+        this.ownerId = ownerId;
+        this.ownerName = ownerName;
+        this.height = height;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.isDeleted = isDeleted;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
+}
