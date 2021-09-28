c8b8be0dcbd44b534702e50dafda512a17fd4b7f
onirutlA <alturino001@gmail.com>
2021-09-28 07:32:24 +0700

    fix: add autowired to constructor

--- src/main/java/com/onirutla/catanddogapi/behaviors/cat/command/InsertCat.java
+++ src/main/java/com/onirutla/catanddogapi/behaviors/cat/command/InsertCat.java
@@ -3,6 +3,7 @@ package com.onirutla.catanddogapi.behaviors.cat.command;
 import com.onirutla.catanddogapi.behaviors.BaseCommand;
 import com.onirutla.catanddogapi.model.Cat;
 import com.onirutla.catanddogapi.repository.CatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
@@ -10,7 +11,7 @@ public class InsertCat implements BaseCommand<Cat> {
 
     private final CatRepository repository;
 
-    public InsertCat(CatRepository repository) {
+    public InsertCat(@Autowired CatRepository repository) {
         this.repository = repository;
     }
 
