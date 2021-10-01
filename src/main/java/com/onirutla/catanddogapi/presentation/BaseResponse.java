4c8a8092dbc0186dbff3df0aa36dbfb6b37f1315
onirutlA <alturino001@gmail.com>
2021-10-01 15:16:33 +0700

    add base response

---
+++ src/main/java/com/onirutla/catanddogapi/presentation/BaseResponse.java
@@ -0,0 +1,33 @@
+package com.onirutla.catanddogapi.presentation;
+
+public class BaseResponse<T> {
+
+    private String statusCode;
+    private String message;
+    private T result;
+
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+}
